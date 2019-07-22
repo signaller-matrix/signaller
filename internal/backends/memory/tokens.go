213abebf0acc0a414a61d07a8afb17ce98bb4e3d
nxshock <nxshock@gmail.com>
2019-07-22 22:28:55 +0500

    Remove error check for token generator

--- internal/backends/memory/tokens.go
+++ internal/backends/memory/tokens.go
@@ -8,10 +8,7 @@ import (
 // newToken returns new generated token with specified length
 func newToken(size int) string {
 	b := make([]byte, size)
-	_, err := rand.Read(b)
-	if err != nil {
-		panic(err)
-	}
+	rand.Read(b) // TODO: check may be can be error
 
 	return fmt.Sprintf("%x", b)
 }
